12cdf25b0b57acbb6b6e43bbcd6909c8f66ad03d
Vladislav <86352975+VladOldProgram@users.noreply.github.com>
2022-10-06 18:26:38 +0300

    Add files via upload

---
+++ ExtensionsTask.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_slideviews
+{
+	public static class ExtensionsTask
+	{
+		/// <summary>
+		/// Медиана списка из нечетного количества элементов — это серединный элемент списка после сортировки.
+		/// Медиана списка из четного количества элементов — это среднее арифметическое 
+        /// двух серединных элементов списка после сортировки.
+		/// </summary>
+		/// <exception cref="InvalidOperationException">Если последовательность не содержит элементов</exception>
+		public static double Median(this IEnumerable<double> items)
+		{
+			var itemsList = items.ToList();
+			itemsList.Sort();
+
+			int listLength = itemsList.Count;
+			if (listLength == 0)
+				throw new InvalidOperationException();
+			else if (listLength % 2 == 0)
+				return (itemsList[listLength / 2 - 1] + itemsList[listLength / 2]) / 2;
+			else
+				return itemsList[listLength / 2];
+		}
+
+		/// <returns>
+		/// Возвращает последовательность, состоящую из пар соседних элементов.
+		/// Например, по последовательности {1,2,3} метод должен вернуть две пары: (1,2) и (2,3).
+		/// </returns>
+		public static IEnumerable<Tuple<T, T>> Bigrams<T>(this IEnumerable<T> items)
+		{
+			T firstItem = default;
+			bool firstItemIsTaken = true;
+			foreach (var item in items)
+			{
+				if (firstItemIsTaken)
+				{
+					firstItem = item;
+					firstItemIsTaken = false;
+				}
+				else
+				{
+					yield return new Tuple<T, T>(firstItem, item);
+					firstItem = item;
+				}
+			}
+		}
+	}
+}
---
+++ StatisticsTask.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace linq_slideviews
+{
+	public class StatisticsTask
+	{
+		public static double GetMedianTimePerSlide(List<VisitRecord> visits, SlideType slideType)
+		{
+            var visitsList = visits.ToList();
+
+            if (!visits.Any()) return 0.0;
+
+            visitsList.Sort((first, second) =>
+            {
+                var r1 = first.UserId.CompareTo(second.UserId);
+                var r2 = first.DateTime.CompareTo(second.DateTime);
+                return (r1 != 0) ? r1 : r2;
+            });
+
+            var filteredVisitsList = visitsList.Bigrams()
+                .Where(tuple => tuple.Item1.UserId == tuple.Item2.UserId 
+                    && tuple.Item1.SlideId != tuple.Item2.SlideId
+                    && tuple.Item1.SlideType == slideType)
+                .Select(tuple => tuple.Item2.DateTime.Subtract(tuple.Item1.DateTime).TotalMinutes)
+                .Where(time => (1.0 <= time && time <= 120.0))
+                .ToList();
+
+            if (!filteredVisitsList.Any()) return 0.0;
+
+            return filteredVisitsList.Median();
+        }
+	}
+}
